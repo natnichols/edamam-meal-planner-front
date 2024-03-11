2effbcbdbf70ce4acd7a35717648418414b67884
NatNichols <natalie.e.nichols@gmail.com>
2024-03-11 15:25:47 -0400

    adjust map fn

--- src/pages/RecipeIndex/RecipeIndex.jsx
+++ src/pages/RecipeIndex/RecipeIndex.jsx
@@ -8,7 +8,7 @@ const RecipeIndex = (props) => {
   return ( 
     <div className={styles.recipeContainer}>
       <h1>My Recipes</h1>
-      {props.profile.recipes.map(recipe => 
+      {props.profile.recipes?.map(recipe => 
         <NavLink to={`/recipes/${recipe.edamamId}`} key={recipe.edamamId}>
           <h2>{recipe.title}</h2>
         </NavLink>
